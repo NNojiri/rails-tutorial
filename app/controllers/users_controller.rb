71b34882ecdf4201baa7e5f879e0bac2920358f2
Naruhiko_Nojiri <n.nojiri0620@gmail.com>
2018-08-06 14:44:50 +0000

    enable sign up function.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def create
-    @user = User.new(params[:id])
+    @user = User.new(user_params)
     if @user.save
+      redirect_to @user
     else
       render 'new'
     end
