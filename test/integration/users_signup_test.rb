47f4fafe2efd3bff98490604174cc8a22102509e
Naruhiko_Nojiri <n.nojiri0620@gmail.com>
2018-08-07 15:18:17 +0000

    create a valid sign up test case and finish user signup.

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid singup information" do
+  
+  test "invalid signup information" do
     get signup_path
     assert_no_difference "User.count" do
     post users_path, params: { user: {
@@ -14,5 +15,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
+  end
+  
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
   end
 end
