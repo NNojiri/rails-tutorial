326f1a3cb810a8bc02a623315a8e450b4ece71bf
Naruhiko_Nojiri <n.nojiri0620@gmail.com>
2018-09-24 10:34:49 +0000

    Make mail preview function valid in develop env and fix misspell.

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  test "accuount_activation" do
-    mail = UserMailer.accuount_activation
+  test "account_activation" do
+    mail = UserMailer.account_activation
     assert_equal "Accuount activation", mail.subject
     assert_equal ["to@example.org"], mail.to
     assert_equal ["from@example.com"], mail.from
