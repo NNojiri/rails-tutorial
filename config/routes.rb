3e023183f00212cc25b8b5c75f711c2676efb7a9
Naruhiko_Nojiri <n.nojiri0620@gmail.com>
2018-09-26 00:10:18 +0000

    Generate Password reset controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#home"
   get '/help', to:'static_pages#help'
