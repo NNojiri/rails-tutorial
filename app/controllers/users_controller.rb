2b1b5fb837c3a7a6e3b4f032f295509619a1096e
Naruhiko_Nojiri <n.nojiri0620@gmail.com>
2018-09-30 11:29:12 +0000

    Restrict access to microposts actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user?, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user?, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
   
@@ -53,14 +53,6 @@ class UsersController < ApplicationController
     end
     
     #以下、before アクション
-    def logged_in_user?
-      unless logged_in?
-        store_location
-        flash[:danger] = 'Please log in.'
-        redirect_to login_url
-      end
-    end
-    
     def correct_user?
       @user = User.find(params[:id])
       redirect_to root_url unless current_user?(@user)
